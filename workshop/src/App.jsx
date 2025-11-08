7b42a83be9c7567561ca483580f828199cb94156
Ratan Sathwik Billakanti <145991418+RatanSathwik@users.noreply.github.com>
2025-11-08 10:14:31 +0000

    initial commit

---
+++ workshop/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Greet from "./component/greet"
+
+function App(){
+  return (
+    <>
+      <h1>hello</h1>
+      <Greet/>
+    </>
+  );
+}
+
+export default App;
---
+++ workshop/src/component/greet.jsx
@@ -0,0 +1,10 @@
+
+
+
+function Greet(props){
+    const { name } = props;
+    let a = name;
+    return a;
+}
+
+export default Greet;
